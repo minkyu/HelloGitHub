b1d1d20364cfaa5d6644d678fcf44c519f0130fa
daliot <tantara3@dreamwiz.com>
2011-09-24 10:27:21 +0900

    new xcode project
    그냥 해 봤습니다.

---
+++ iPadProject/iPadProject/RootViewController.h
@@ -0,0 +1,24 @@
+//
+//  RootViewController.h
+//  iPadProject
+//
+//  Created by 오 화종 on 11. 09. 24.
+//  Copyright 2011년 앱달. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (nonatomic, retain) IBOutlet DetailViewController *detailViewController;
+
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+- (void)insertNewObject:(id)sender;
+
+@end
