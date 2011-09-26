855117749fe51c71da79b898a70a24d087f66384
민규 최 <Min@TEAMBLACKCAT.local>
2011-09-26 20:32:24 +0900

    충돌 해결

---
+++ iPadProject/iPadProject/DetailViewController.h
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.h
+//  iPadProject
+//
+//  Created by 오 화종 on 11. 09. 24.
+//  Copyright 2011년 앱달. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@class RootViewController;
+
+@interface DetailViewController : UIViewController <UIPopoverControllerDelegate, UISplitViewControllerDelegate>
+{
+	NSTimer *faceTimer;
+	
+	int winksLeft;
+}
+
+@property (nonatomic, retain) NSTimer *faceTimer;
+
+@property (nonatomic, retain) IBOutlet UIToolbar *toolbar;
+
+@property (nonatomic, retain) NSManagedObject *detailItem;
+
+@property (nonatomic, retain) IBOutlet UILabel *detailDescriptionLabel;
+
+@property (nonatomic, assign) IBOutlet RootViewController *rootViewController;
+
+- (IBAction)insertNewObject:(id)sender;
+- (IBAction)wakeUp:(id)sender;
+
+@end
---
+++ iPadProject/iPadProjectTests/iPadProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  iPadProjectTests.m
+//  iPadProjectTests
+//
+//  Created by 오 화종 on 11. 09. 24.
+//  Copyright 2011년 앱달. All rights reserved.
+//
+
+#import "iPadProjectTests.h"
+
+@implementation iPadProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iPadProjectTests");
+}
+
+@end
